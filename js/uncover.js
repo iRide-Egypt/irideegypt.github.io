36aed1497ee463ee8c4687bb525c9a295fdcb4a5
thassan <sohayb.muhammed@capgemini.com>
2021-01-05 21:46:25 +0100

    Remove useless

--- js/uncover.js
+++ js/uncover.js
@@ -1,13 +1,4 @@
-/**
- * uncover.js
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2018, Codrops
- * http://www.codrops.com
- */
+
 {
     class Uncover {
         constructor(el, options) {
